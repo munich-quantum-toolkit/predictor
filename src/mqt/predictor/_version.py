bda1604932a684bf4b0a75f047be8f7f55d9499b
antotu <anto.tu98@hotmail.it>
2025-09-08 12:30:58 +0200

    Fixed bug Swap and Cswap gates

--- src/mqt/predictor/_version.py
+++ src/mqt/predictor/_version.py
@@ -34,7 +34,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "2.3.1.dev6+g1d835bd4c"
-__version_tuple__ = version_tuple = (2, 3, 1, "dev6", "g1d835bd4c")
+__version__ = version = "0.1.dev719+g5ea17201a.d20250908"
+__version_tuple__ = version_tuple = (0, 1, "dev719", "g5ea17201a.d20250908")
 
 __commit_id__ = commit_id = None
