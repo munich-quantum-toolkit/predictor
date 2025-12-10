ba6351d2c537b0334a67d6076c67268222489a45
antonio.tudisco <s285433@studenti.polito.it>
2025-12-10 10:36:08 +0100

    Reduced ignore on warning, modified doc, and minor fixes

--- src/mqt/predictor/ml/predictor.py
+++ src/mqt/predictor/ml/predictor.py
@@ -332,7 +332,7 @@ class Predictor:
         )
         for sample in results:
             training_sample, circuit_name, scores = sample
-            if all(score == -1 for score in scores):
+            if all(score == -1 for score in scores.values()):
                 continue
 
             if self.gnn:
