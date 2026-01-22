f34a9ae70dd6bcc8528670fed7941edca1239b1f
flowerthrower <flowerthrower@users.noreply.github.com>
2026-01-22 10:21:07 +0100

    disable progress bar

--- src/mqt/predictor/rl/predictor.py
+++ src/mqt/predictor/rl/predictor.py
@@ -104,11 +104,12 @@ class Predictor:
             batch_size = 10
             progress_bar = False
         else:
+            set_random_seed(0)
             # default PPO values
             n_steps = 2048
             n_epochs = 10
             batch_size = 64
-            progress_bar = True
+            progress_bar = False
 
         logger.debug("Start training for: " + self.figure_of_merit + " on " + self.device_name)
         model = MaskablePPO(
